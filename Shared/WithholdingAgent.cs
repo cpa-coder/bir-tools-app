5d5521b073cf4a81120f6fc5b558f04da3a58c30
Ogie Galicia <o.galicia.cpa@gmail.com>
2023-05-14 11:13:51 +0800

    Feat: implement withholding agent page

---
+++ Shared/WithholdingAgent.cs
@@ -0,0 +1,10 @@
+﻿namespace BirToolsApp.Shared;
+
+public class WithholdingAgent
+{
+    public string Name { get; set; }
+    public string Rdo { get; set; }
+    public string Type { get; set; }
+    public DateTime DatePublished { get; set; }
+    public string Status { get; set; }
+}
